2d451a0423f2dee547b6424f09fd8f821174484c
Carlos Pena Caballero <andrescapc@aol.com>
2022-07-21 11:15:14 -0500

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,14 +1,7 @@
 require 'sinatra'
 
 class MyApp < Sinatra::Base
-    get "/hello" do
-        erb :hello_form
-    end
-    
-    post "/hello" do
-        @name = params["name"]
-        erb :hello
-    end
+
 end
 
 
